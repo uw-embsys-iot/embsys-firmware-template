ba75de4e125c44a38ae9c49d14f102b8f10e0d8c
Mikhail Skobov <mskobov@lyft.com>
2024-01-19 11:53:49 -0800

    Assignment 1 comments

--- app/src/main.c
+++ app/src/main.c
@@ -4,7 +4,6 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-/* IOTEMBSYS: Add required headers for shell and/or others */
 #include <zephyr/shell/shell.h>
 
 #include <stdlib.h>
@@ -17,28 +16,6 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
-/* IOTEMBSYS2: Add joystick key declarations/references from the device tree. */
-#define SW0_NODE	DT_ALIAS(sw0)
-#define SW1_NODE	DT_ALIAS(sw1)
-#define SW2_NODE	DT_ALIAS(sw2)
-#define SW3_NODE	DT_ALIAS(sw3)
-#define SW4_NODE	DT_ALIAS(sw4)
-static const struct gpio_dt_spec sw0 = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
-							      {0});
-static const struct gpio_dt_spec sw1 = GPIO_DT_SPEC_GET_OR(SW1_NODE, gpios,
-							      {0});
-static const struct gpio_dt_spec sw2 = GPIO_DT_SPEC_GET_OR(SW2_NODE, gpios,
-							      {0});
-static const struct gpio_dt_spec sw3 = GPIO_DT_SPEC_GET_OR(SW3_NODE, gpios,
-							      {0});								  								  								  
-static const struct gpio_dt_spec sw4 = GPIO_DT_SPEC_GET_OR(SW4_NODE, gpios,
-							      {0});								  
-static struct gpio_callback button_cb_data_0;
-static struct gpio_callback button_cb_data_1;
-static struct gpio_callback button_cb_data_2;
-static struct gpio_callback button_cb_data_3;
-static struct gpio_callback button_cb_data_4;
-
 /*
  * A build error on this line means your board is unsupported.
  * See the blinky sample documentation for information on how to fix this.
@@ -48,71 +25,7 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 /* The amount of time between GPIO blinking. */
 static uint32_t blink_interval_ = DEFAULT_SLEEP_TIME_MS;
 
-static void change_blink_interval(uint32_t new_interval_ms) {
-	blink_interval_ = new_interval_ms;
-}
-
-/* IOTEMBSYS2: Add joystick press handler. Metaphorical bonus points for debouncing. */
-static void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins) {
-	// Sophomoric "debouncing" implementation
-	printk("Button %d pressed at %" PRIu32 "\n", pins, k_cycle_get_32());
-	k_msleep(100);
-
-	uint32_t interval_ms = 0;
-	if (pins == BIT(sw0.pin)) {
-		interval_ms = 100;
-	} else if (pins == BIT(sw1.pin)) {
-		// Down
-		interval_ms = 200;
-	} else if (pins == BIT(sw2.pin)) {
-		// Right
-		interval_ms = 500;
-	} else if (pins == BIT(sw3.pin)) {
-		// Up
-		interval_ms = 1000;
-	} else if (pins == BIT(sw4.pin)) {
-		// Left
-		interval_ms = 2000;
-	} else {
-		printk("Unrecognized pin");
-	}
-
-	if (interval_ms != 0) {
-		printk("Setting interval to %d\n", interval_ms);
-		change_blink_interval(interval_ms);
-	}
-}
-
-static int init_joystick_gpio(const struct gpio_dt_spec* button, struct gpio_callback* data) {
-	int ret = -1;
-
-	if (!gpio_is_ready_dt(button)) {
-		printk("Error: button device %s is not ready\n",
-		       button->port->name);
-		return ret;
-	}
-
-	ret = gpio_pin_configure_dt(button, GPIO_INPUT);
-	if (ret != 0) {
-		printk("Error %d: failed to configure %s pin %d\n",
-		       ret, button->port->name, button->pin);
-		return ret;
-	}
-
-	ret = gpio_pin_interrupt_configure_dt(button,
-					      GPIO_INT_EDGE_TO_ACTIVE);
-	if (ret != 0) {
-		printk("Error %d: failed to configure interrupt on %s pin %d\n",
-			ret, button->port->name, button->pin);
-		return ret;
-	}
-
-	gpio_init_callback(data, button_pressed, BIT(button->pin));
-	gpio_add_callback(button->port, data);
-	return ret;
-}
-
+/* IOTEMBSYS1: This is a simple "blinky" app that we will be developing on top of. */
 void main(void)
 {
 	int ret;
@@ -126,39 +39,16 @@ void main(void)
 		return;
 	}
 
-	/* IOTEMBSYS2: Configure joystick GPIOs. Each gpio must be configured as GPIO_INPUT and have a callback handler. */
-	init_joystick_gpio(&sw0, &button_cb_data_0);
-	init_joystick_gpio(&sw1, &button_cb_data_1);
-	init_joystick_gpio(&sw2, &button_cb_data_2);
-	init_joystick_gpio(&sw3, &button_cb_data_3);
-	init_joystick_gpio(&sw4, &button_cb_data_4);
-
-	LOG_INF("Running blinky");
+	/* IOTEMBSYS1: printk is just like printf. */	
+	printk("Running blinky\n");
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
-		/* IOTEMBSYS2: Print the LED GPIO state to console. */
-		printk("LED state: %d\n", gpio_pin_get_dt(&led));
+
+		/* IOTEMBSYS1: LOG_X are like printf, except they include additional details in logs. */
+		LOG_INF("LED state: %d", gpio_pin_get_dt(&led));
 		if (ret < 0) {
 			return;
 		}
 		k_msleep(blink_interval_);
 	}
 }
-
-/* IOTEMBSYS2: Add shell commands and handler. */
-static int cmd_demo_blink(const struct shell *shell, size_t argc, char **argv)
-{
-	uint32_t interval_ms = atoi(argv[1]);
-	if (interval_ms == 0) {
-		printk("Invalid interval\n");
-	}
-	shell_print(shell, "Setting interval to: %d", interval_ms);
-
-	change_blink_interval(interval_ms);
-	return 0;
-}
-
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_app,
-	SHELL_CMD(blink, NULL, "Change blink interval", cmd_demo_blink),
-);
-SHELL_CMD_REGISTER(app, &sub_app, "Application commands", NULL);
